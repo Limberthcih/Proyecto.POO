68e115a6d77a2a064b30f74aaabb36102ed38d1e
Alex-May <36852798+Alex-May@users.noreply.github.com>
2018-07-04 14:01:13 -0500

    Baja de una venta

---
+++ Proyecto/src/Controlador/ControlBajaVentas.java
@@ -0,0 +1,17 @@
+package Controlador;
+
+import Proyecto.Vista.Bajas;
+import java.util.Scanner;
+
+public class ControlBajaVentas{
+    //Variables
+    
+    public void EliminarVenta(){
+        //clear(Usuario);
+    }
+    
+    public int BúsquedaVenta(){
+        //return Usuario;
+    }
+}
+
