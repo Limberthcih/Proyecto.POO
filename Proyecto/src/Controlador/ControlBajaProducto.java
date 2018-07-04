f1151c7f751a8e7707744a3767ff841c9c16cabb
Alex-May <36852798+Alex-May@users.noreply.github.com>
2018-07-04 14:02:41 -0500

    Baja de un producto

---
+++ Proyecto/src/Controlador/ControlBajaProducto.java
@@ -0,0 +1,16 @@
+package Controlador;
+
+import Proyecto.Vista.Bajas;
+import java.util.Scanner;
+
+public class ControlBajaProducto{
+    //Variables
+    
+    public void EliminarProducto(){
+        //clear(producto)
+    }
+    
+    public void BusquedaProducto(){
+        //return producto;
+    }
+}
