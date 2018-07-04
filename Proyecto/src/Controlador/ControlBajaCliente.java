40b9563564d6a60a79967eebca4a4fc1dd928466
Alex-May <36852798+Alex-May@users.noreply.github.com>
2018-07-04 14:03:46 -0500

    Baja de un cliente

---
+++ Proyecto/src/Controlador/ControlBajaCliente.java
@@ -0,0 +1,16 @@
+package Controlador;
+
+import Proyecto.Vista.Bajas;
+import java.util.Scanner;
+
+public class ControlBajaCliente{
+    //Variables
+    
+    public void EliminarCliente(){
+        //clear(cliente)
+    }
+    
+    public void BusquedaCliente(){
+        //return cliente;
+    }
+}
