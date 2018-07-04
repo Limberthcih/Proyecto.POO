66288fc78a5ae7faffff103f5efab0bdb5e2144d
Alex-May <36852798+Alex-May@users.noreply.github.com>
2018-07-04 13:58:51 -0500

    Baja de un usuario

---
+++ Proyecto/src/Controlador/ControlBajaUsuario.java
@@ -0,0 +1,17 @@
+package Controlador;
+
+import Proyecto.Vista.Bajas;
+import java.util.Scanner;
+
+public class ControlBajaUsuario{
+    //Variables
+    
+    public void EliminarUsuario(){
+        //clear(Usuario);
+    }
+    
+    public int BúsquedaUsuario(){
+        //return Usuario;
+    }
+}
+
