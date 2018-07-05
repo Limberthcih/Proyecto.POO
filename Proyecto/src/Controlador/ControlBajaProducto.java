e6cafc14fd50c94bf5f5f817041fa0a8acefdbba
Alex-May <36852798+Alex-May@users.noreply.github.com>
2018-07-05 15:32:58 -0500

    Update ControlBajaProducto.java

--- Proyecto/src/Controlador/ControlBajaProducto.java
+++ Proyecto/src/Controlador/ControlBajaProducto.java
@@ -6,11 +6,51 @@ import java.util.Scanner;
 public class ControlBajaProducto{
     //Variables
     
-    public void EliminarProducto(){
-        //clear(producto)
+     public void EliminarProducto(String nombre, String clave){
+        ArrayList<String> buscador = new ArrayList<String>; //Se debe meter el id por aquí
+        FileWriter fr = null;
+        BufferedReader br = null;
+        
+        try{
+            fr = new FileWriter(nombre);
+            br = new BufferedWriter(fr);
+            for(String clave:buscador){
+                if(productos.clave!=clave){
+                    bw.write(productos.clave + "\t"+productos.descripcion+ "\t"+ productos.preciounitario+ "\t"+ productos.cantidad+"\r\n");
+                }else{
+                    System.out.println("El producto ha sido eliminado");
+                }
+            }
+            
+            
+        }catch(Exception e){
+            System.out.println(ex.getMessage());  
+        }finally{
+            bw.close();
+            buscador.clear();
+        }
     }
     
-    public void BusquedaProducto(){
-        //return producto;
+    public void BusquedaProducto(String nombre, String id){
+        ArrayList<String> buscador = new ArrayList<String>; 
+        FileWriter fr = null;
+        PrintWriter pw = null;
+        
+        try{
+            fr = new FileWriter(nombre);
+            br = new PrintdWriter(fr);
+            for(String rfc:buscador){
+                if(productos.clave==id){
+                    pw.println(productos.clave + "\t"+productos.descripcion+ "\t"+ productos.preciounitario+ "\t"+ productos.cantidad+"\r\n");
+                }
+            }
+            
+            
+        }catch(Exception e){
+            System.out.println(ex.getMessage());  
+        }finally{
+            bw.close();
+            //buscador.clear();
+        }
     }
 }
