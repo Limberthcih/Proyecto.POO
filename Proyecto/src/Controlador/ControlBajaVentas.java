e1548f41b1d9e809b494b6395eb9a0f000950769
Alex-May <36852798+Alex-May@users.noreply.github.com>
2018-07-05 15:42:54 -0500

    Update ControlBajaVentas.java

--- Proyecto/src/Controlador/ControlBajaVentas.java
+++ Proyecto/src/Controlador/ControlBajaVentas.java
@@ -5,13 +5,54 @@ import java.util.Scanner;
 
 public class ControlBajaVentas{
     //Variables
-    
-    public void EliminarVenta(){
-        //clear(Usuario);
+    //https://mega.nz/#!gw4TEbLS!dpHMlvc380rq4hngAOkHlSX9dZOI3iQHlYQCiL6I9io
+    public void EliminarVenta(String nombre, String clave){
+        ArrayList<String> buscador = new ArrayList<String>; //Se debe meter el id por aquí
+        FileWriter fr = null;
+        BufferedReader br = null;
+        
+        try{
+            fr = new FileWriter(nombre);
+            br = new BufferedWriter(fr);
+            for(String clave:buscador){
+                if(venta.id!=clave){
+                    bw.write(venta.id + "\t"+venta.fecha+ "\t"+ venta.cliente/*cliente.nombre*/+ "/t" +venta.total+ "\r\n");
+                }else{
+                    System.out.println("La venta ha sido eliminada");
+                }
+            }
+            
+            
+        }catch(Exception e){
+            System.out.println(ex.getMessage());  
+        }finally{
+            bw.close();
+            buscador.clear();
+        }
     }
     
-    public int B�squedaVenta(){
-        //return Usuario;
+    public void BusquedaVenta(String nombre, String id){
+        ArrayList<String> buscador = new ArrayList<String>; 
+        FileWriter fr = null;
+        PrintWriter pw = null;
+        
+        try{
+            fr = new FileWriter(nombre);
+            br = new PrintdWriter(fr);
+            for(String id:buscador){
+                if(venta.id==id){
+                    pw.println(venta.id + "\t"+venta.fecha+ "\t"+ venta.cliente/*cliente.nombre*/+ "/t" +venta.total+ "\r\n");
+                    //¿Se pedirá más informació?
+                }
+            }
+            
+            
+        }catch(Exception e){
+            System.out.println(ex.getMessage());  
+        }finally{
+            bw.close();
+            //buscador.clear();
+        }
     }
 }
 
