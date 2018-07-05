2c7e3b555c32af8ede81f4e3d880a7abff821760
Alex-May <36852798+Alex-May@users.noreply.github.com>
2018-07-05 15:36:08 -0500

    Update ControlBajaUsuario.java

--- Proyecto/src/Controlador/ControlBajaUsuario.java
+++ Proyecto/src/Controlador/ControlBajaUsuario.java
@@ -3,15 +3,52 @@ package Controlador;
 import Proyecto.Vista.Bajas;
 import java.util.Scanner;
 
-public class ControlBajaUsuario{
-    //Variables
-    
-    public void EliminarUsuario(){
-        //clear(Usuario);
+        public void EliminarUsuario(String nombre, String clave){
+        ArrayList<String> buscador = new ArrayList<String>; //Se debe meter el id por aquí
+        FileWriter fr = null;
+        BufferedReader br = null;
+        
+        try{
+            fr = new FileWriter(nombre);
+            br = new BufferedWriter(fr);
+            for(String clave:buscador){
+                if(usuario.nombre!=clave){
+                    bw.write(usuario.nombre + "\t"+usuario.contraseña + "\t"+ usuario.rol+ "\r\n");
+                }else{
+                    System.out.println("El usuario ha sido eliminado");
+                }
+            }
+            
+            
+        }catch(Exception e){
+            System.out.println(ex.getMessage());  
+        }finally{
+            bw.close();
+            buscador.clear();
+        }
     }
     
-    public int B�squedaUsuario(){
-        //return Usuario;
+    public void BusquedaUsuario(String nombre, String id){
+        ArrayList<String> buscador = new ArrayList<String>; 
+        FileWriter fr = null;
+        PrintWriter pw = null;
+        
+        try{
+            fr = new FileWriter(nombre);
+            br = new PrintdWriter(fr);
+            for(String rfc:buscador){
+                if(usuario.nombre==id){
+                    pw.println(usuario.nombre + "\t"+usuario.contraseña + "\t"+ usuario.rol+ "\r\n");
+                }
+            }
+            
+            
+        }catch(Exception e){
+            System.out.println(ex.getMessage());  
+        }finally{
+            bw.close();
+            //buscador.clear();
+        }
     }
 }
 
