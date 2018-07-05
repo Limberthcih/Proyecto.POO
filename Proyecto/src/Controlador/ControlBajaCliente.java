ff910895d162bd52d5f355ce853b6ed9adb85bad
Alex-May <36852798+Alex-May@users.noreply.github.com>
2018-07-05 15:25:52 -0500

    Update ControlBajaCliente.java

--- Proyecto/src/Controlador/ControlBajaCliente.java
+++ Proyecto/src/Controlador/ControlBajaCliente.java
@@ -1,16 +1,57 @@
 package Controlador;
 
 import Proyecto.Vista.Bajas;
-import java.util.Scanner;
+import java.util.*;
+import java.io.*;
 
 public class ControlBajaCliente{
     //Variables
     
-    public void EliminarCliente(){
-        //clear(cliente)
+    public void EliminarCliente(String nombre, String rfc){
+        ArrayList<String> buscador = new ArrayList<String>; 
+        FileWriter fr = null;
+        BufferedReader br = null;
+        
+        try{
+            fr = new FileWriter(nombre);
+            br = new BufferedWriter(fr);
+            for(String rfc:buscador){
+                if(clientes.rfc!=rfc){
+                    bw.write(clientes.rfc + "\t"+clientes.nombre+ "\t"+ clientes.dirección+ "\t"+ clientes.telefono+"\r\n");
+                }else{
+                    System.out.println("El producto ha sido eliminado");
+                }
+            }
+            
+            
+        }catch(Exception e){
+            System.out.println(ex.getMessage());  
+        }finally{
+            bw.close();
+            buscador.clear();
+        }
     }
     
-    public void BusquedaCliente(){
-        //return cliente;
+    public void BusquedaCliente(String nombre, String rfc){
+        ArrayList<String> buscador = new ArrayList<String>; 
+        FileWriter fr = null;
+        PrintWriter pw = null;
+        
+        try{
+            fr = new FileWriter(nombre);
+            br = new PrintdWriter(fr);
+            for(String rfc:buscador){
+                if(clientes.rfc==rfc){
+                    pw.println(clientes.rfc + "\t"+clientes.nombre+ "\t"+ clientes.dirección+ "\t"+ clientes.telefono+"\r\n");
+                }
+            }
+            
+            
+        }catch(Exception e){
+            System.out.println(ex.getMessage());  
+        }finally{
+            bw.close();
+            //buscador.clear();
+        }
     }
 }
